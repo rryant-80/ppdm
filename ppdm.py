1757324b1224ef3177650f25591caadc463d87d9
rryant-80 <67359656+rryant-80@users.noreply.github.com>
2026-07-21 15:46:56 +0800

    Adjust title and legend y positions in ppdm.py

--- ppdm.py
+++ ppdm.py
@@ -803,7 +803,7 @@ def render_layanan_pertanahan(df_filtered_layanan):
                 text="Rekapitulasi Berkas Melebihi SOP per Posisi Berkas",
                 font=dict(size=13, color="#2c3e50"),
                 x=0.0,
-                y=0.98,
+                y=0.80,
                 xanchor='left',
                 yanchor='top'
             ),
@@ -811,7 +811,7 @@ def render_layanan_pertanahan(df_filtered_layanan):
             legend=dict(
                 orientation="h", 
                 yanchor="bottom", 
-                y=0.90, 
+                y=0.95, 
                 xanchor="left", 
                 x=0.0,
                 font=dict(size=8.5)
