77c0901311889b00e0677bdf816d7c00c653876e
rryant-80 <67359656+rryant-80@users.noreply.github.com>
2026-07-22 23:14:36 +0800

    Update ppdm.py

--- ppdm.py
+++ ppdm.py
@@ -807,7 +807,7 @@ def render_layanan_pertanahan(df_filtered_layanan):
             legend=dict(
                 orientation="h", 
                 yanchor="top", 
-                y=0.98, 
+                y=1.08, 
                 xanchor="left", 
                 x=0.0,
                 font=dict(size=8.5)
