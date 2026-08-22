b4b5cbd1d50e0e952a86c8bcd5c7858728599352
rryant-80 <67359656+rryant-80@users.noreply.github.com>
2026-08-22 22:38:20 +0800

    Change color codes for KSA/KPA AIR and TUBUH AIR

--- ppdm.py
+++ ppdm.py
@@ -157,8 +157,8 @@ def render_peta_kawasan_hutan():
             "#febaa9", #HPK
             "#ad40fe", #KSA/KPA
             "#ad40fe", #KSA/KPA
-            "#ad40fe", #KSA/KPA AIR
-            "#ad40fe", #TUBUH AIR
+            "#6ce6f2", #KSA/KPA AIR
+            "#6ce6f2", #TUBUH AIR
             "#ad40fe", #TWA
             "#17becf",
         ]
